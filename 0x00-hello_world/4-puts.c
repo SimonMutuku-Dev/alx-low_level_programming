81bb15c28479fbe37c67f4ac5f2c5b6bb4078bab
SimonMutuku-Dev <mutukusimonmuasya@gmail.com>
2023-07-17 16:02:57 -0700

    Comment test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /**
- * main - Parent function, returns 0 if expression is true
- * void - accepts no input
- * puts - function that prints text
- * return - returns the value 0 for the 'main' function if the expression is true
+ * main - Prints text "Programming is like building
+ * a multilingual puzzle"
+ * void: accepts no input
+ * puts: function that prints text
+ * return: returns the value 0 for the 'main' function if the expression is true
  */
 int main(void)
 {
