48de4cebe3b839cd1a669d889ffeda6d2f568c8e
SimonMutuku-Dev <mutukusimonmuasya@gmail.com>
2023-07-31 18:57:58 -0700

    First Iteration

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void print_array(int *a, int n);
+void _puts(char *str);
+void puts2(char *str);
+int _atoi(char *s);
+void puts_half(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif /* MAIN_H */
